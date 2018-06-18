d1b5689c3f7f1b49d390c62bb48545de22732a49
Peyman Eskandari <peyman.eskandari@gmail.com>
2018-06-18 13:00:15 +0430

    finish alert

--- src/resources/views/alert.blade.php
+++ src/resources/views/alert.blade.php
@@ -1,3 +1,15 @@
-<div class="alert alert-{{ $type }} {{ $class or '' }}" role="alert">
+<div class="alert
+            alert-{{ $type or 'primary' }}
+            @if( isset($link) && $link ) alert-link @endif
+            @if( isset($dismissible) && $dismissible ) alert-dismissible @endif
+            {{ $class or '' }}"
+            role="alert">
+
+    @if( isset($dismissible) && $dismissible )
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    @endif
+
     {{ $slot }}
 </div>
